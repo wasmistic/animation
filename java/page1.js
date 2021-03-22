b93d5d247b210cad945c6a2e080f924ff7a16fef
wasmistic <wassuccess@ymail.com>
2021-03-21 23:17:17 -0700

    animation pages

---
+++ java/page1.js
@@ -0,0 +1,6 @@
+function toggle(){
+    const toggle=document.querySelector('.toggle');
+    const container=document.querySelector('.container');
+    toggle.classList.toggle('active')
+    container.classList.toggle('active')
+}
---
+++ java/page3.js
@@ -0,0 +1,6 @@
+function toggle(){
+    const toggle=document.querySelector('.toggle');
+    const slider=document.querySelector('.slider');
+    toggle.classList.toggle('active')
+    slider.classList.toggle('active')
+}
